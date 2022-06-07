b42b93bd91938d98703d314fd02199f293eea6c4
htmlpluscss <79198889134@ya.ru>
2022-06-07 22:02:29 +0300

    faq

--- src/js/js.js
+++ src/js/js.js
@@ -18,4 +18,13 @@ https://github.com/htmlpluscss/
 
 	});
 
+	// обработчик анимаций
+	window.cssAnimation = a=>{let b,c,d=document.createElement("cssanimation");switch(a){case'animation':b={"animation":"animationend","OAnimation":"oAnimationEnd","MozAnimation":"animationend","WebkitAnimation":"webkitAnimationEnd"};break;case'transition':b={"transition":"transitionend","OTransition":"oTransitionEnd","MozTransition":"transitionend","WebkitTransition":"webkitTransitionEnd"}}for(c in b)if(d.style[c]!==undefined)return b[c]}
+
+	// Determine if an element is in the visible viewport
+	window.isInViewport = el => {
+		const rect = el.getBoundingClientRect();
+		return (rect.top >= 0 && rect.bottom <= window.innerHeight);
+	}
+
 })();
